c3a04b7a328103b4afb13bb88092c8cd8579d92d
joaquin <j>
2021-11-14 21:18:40 -0800

    multiprocessing

--- grating_coupler_meep/fiber_sweep_fiber_angle.py
+++ grating_coupler_meep/fiber_sweep_fiber_angle.py
@@ -1,11 +1,10 @@
 import subprocess
 import multiprocessing
-from functools import partial
 
 
 def run(
-    value: float,
     key: str,
+    value: float,
     ncores: int = 6,
 ):
     command = f"mpirun -np {ncores} python fiber.py --{key}={value}"
@@ -13,13 +12,14 @@ def run(
     subprocess.call(command, shell=True)
 
 
-run_fiber_xposition = partial(run, key="fiber_xposition")
-run_fiber_angle_deg = partial(run, key="fiber_angle_deg")
-
 if __name__ == "__main__":
     p = multiprocessing.Pool(multiprocessing.cpu_count())
-    p.starmap(run_fiber_angle_deg, fiber_angle_deg=range(10, 30, 5))
+    # p.starmap(
+    #     run_fiber_xposition,
+    #     [("fiber_xposition", fiber_xposition) for fiber_xposition in range(-5, 5)],
+    # )
 
-    # for fiber_xposition in range(-5, 6):
-    #     p = multiprocessing.Process(target=run, args=(fiber_xposition=fiber_xposition))
-    #     p.start()
+    p.starmap(
+        run_fiber_angle_deg,
+        [("fiber_angle_deg", fiber_angle_deg) for fiber_angle_deg in range(10, 30, 5)],
+    )
--- grating_coupler_meep/fiber_sweep_fiber_xposition2.py
+++
@@ -1,6 +0,0 @@
-from subprocess_tee import run
-
-if __name__ == "__main__":
-    for fiber_xposition in range(-5, 6):
-        command = f"mpirun -np {6} python fiber.py --fiber_xposition={fiber_xposition} > log.log"
-        run(command)
